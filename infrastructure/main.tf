2f1b275376b2febbefd88ffb7a8e02ec6f51ba80
Rodney Mach <rodney.mach123@gmail.com>
2025-11-06 13:43:42 -0500

    Removed dynamo

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "my-terraform-state-bucket"
     key            = "infrastructure/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
   required_providers {
