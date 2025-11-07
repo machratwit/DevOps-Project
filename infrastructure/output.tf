cb9b87ed776a7a257269bd77cf049ab26b577423
Rodney Mach <rodney.mach123@gmail.com>
2025-11-07 13:15:15 -0500

    output

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,23 +1,23 @@
 output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.app_server.public_ip
-  sensitive   = true
+  sensitive   = false
 }
 
 output "instance_public_dns" {
   description = "Public DNS of the EC2 instance"
   value       = aws_instance.app_server.public_dns
-  sensitive   = true
+  sensitive   = false
 }
 
 output "ecr_repository_url" {
   description = "URL of the ECR repository to push Docker images"
   value       = aws_ecr_repository.app.repository_url
-  sensitive   = true
+  sensitive   = false
 }
 
 output "security_group_id" {
   description = "ID of the security group used by the instance"
   value       = aws_security_group.ec2_sg.id
-  sensitive   = true
+  sensitive   = false
 }
